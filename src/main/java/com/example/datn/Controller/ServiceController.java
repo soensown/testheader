a25608600cb073a08a1ccde724aa5a7dfcbd3b74
mrsown <soensown@gmail.com>
2025-05-27 13:06:17 +0700

    first commit

---
+++ src/main/java/com/example/datn/Controller/ServiceController.java
@@ -0,0 +1,22 @@
+package com.example.datn.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ServiceController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("newOrders", 57);
+        model.addAttribute("onHoldOrders", 5);
+        model.addAttribute("outOfStock", 15);
+        model.addAttribute("totalOrders", 16247);
+        model.addAttribute("newCustomers", 356);
+        model.addAttribute("couponUsage", 72);
+        model.addAttribute("payingCustomerPercent", 30);
+        return "/component/Overview";
+    }
+}
