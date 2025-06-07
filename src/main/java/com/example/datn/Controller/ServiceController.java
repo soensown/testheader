3202007d01b156db5151679a5896166aa1284469
mrsown <soensown@gmail.com>
2025-06-07 21:52:42 +0700

    ListProduct commit

--- src/main/java/com/example/datn/Controller/ServiceController.java
+++ src/main/java/com/example/datn/Controller/ServiceController.java
@@ -12,6 +12,17 @@ public class ServiceController {
     public String home(Model model){
         return "/page/home";
     }
+    @GetMapping("/DirectSales")
+    public String DirectSales(Model model){
+        model.addAttribute("newOrders", 57);
+        model.addAttribute("onHoldOrders", 5);
+        model.addAttribute("outOfStock", 15);
+        model.addAttribute("totalOrders", 16247);
+        model.addAttribute("newCustomers", 356);
+        model.addAttribute("couponUsage", 72);
+        model.addAttribute("payingCustomerPercent", 30);
+        return "/page/DirectSales";
+    }
     @GetMapping("/overview")
     public String overview(Model model){
         model.addAttribute("newOrders", 57);
