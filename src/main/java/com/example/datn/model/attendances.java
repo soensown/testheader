846cbeb4f39547b9bbd32ae3f74ace26aa8a1104
huathanh2002 <131847539+huathanh2002@users.noreply.github.com>
2025-06-08 14:43:19 +0700

    hqthanh push code model attendances

--- src/main/java/com/example/datn/model/attendances.java
+++ src/main/java/com/example/datn/model/attendances.java
@@ -1,4 +1,15 @@
 package com.example.datn.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "attendances")
 public class attendances {
 }
