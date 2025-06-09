5c15957634d3ccb4eea8e21649c90639a000e87b
mrsown <soensown@gmail.com>
2025-06-09 20:34:18 +0700

    test branch

--- src/main/java/com/example/datn/repository/common_codesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datn.repository;
-
-import com.example.datn.Model.common_codes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface common_codesRepository extends JpaRepository<common_codes, Integer> {
-}
